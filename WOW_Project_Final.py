9939317633603e51ed918233ac1aadc5dabe842d
Carlos Sanchez <carlos.sanchez.1575@gmail.com>
2020-12-20 21:14:00 -0500

    final comments

--- WOW_Project_Final.py
+++ WOW_Project_Final.py
@@ -1,4 +1,5 @@
 # Sudoku Generator
+
 # Our Sudoku board is generated as a nested list (i.e a list of
 # 9 lists with 9 numbers within each list.) Our generator creates
 # a 9 X 9 matrix consisting of nine 3 X 3 smaller matrices.     
@@ -12,37 +13,41 @@ rBase = range(base)
 symbol = " 123456789"
 
 
-# Pattern for a baseline valid solution. Our pattern will assign
-# the index of each inner and outer list 3 X 3 at a time from left
-# to right.  
-
+# Pattern for a baseline puzzle grid, which we will use later when we create
+# our valid solution. Using our base and side variables, our pattern function 
+# will format our list of lists.
+   
 def pattern(r,c):
 	return (base*(r%base)+r//base+c)%side
 
-# We define a shuffle function using the sample mathod from random
-# The sample() methon that returns a list of unique elements chosen
-# randomly from the list provided. The randomly selected list consists
-# of a specified number of items provided.
+# We define a shuffle function using the sample method from random.
+# The sample() method returns a list of unique elements chosen
+# randomly from the list provided without repetition. The randomly 
+# selected list consists of the length of items we provide to the 
+# shuffle function.
 
 def shuffle(s):
 	return sample(s,len(s))
 
-# In this case we enter the range of 1 through 9 for our 9 X 9 sudoku
+# In our nums variable, we provide our shuffle function the range of
+# numbers 1 through one more than our side variable, which we will then
+# shuffle. By definition of our shuffle function the length will be 9 in
+# this case. 
 
-nums  = shuffle(range(1,base*base+1))
+nums  = shuffle(range(1, side + 1))
 
 # We define a grid layout function that we use in conjuction with our 
-# pattern function to create each inner list within our main
-# outer list as well as outer list. We do this by shuffling the numbers
-# 0, 1, 2 multipying them by our base (in this case 3) then adding another
-# random number from 0,1,or 2. This will be used in conjuction with the 
-# pattern function. 
+# pattern function. We use the empy list axis in order to access the numbers
+# we will then use as inputs for our pattern function. Because we are 
+# shuffling our rBase, the numbers that are returned in axis will be random calculations
+# of 0, 1, or 2 times base. 
+
 
 def grid_layout(rBase):
 	axis = []
 	for g in shuffle(rBase):
 		for r in shuffle(rBase):
-			axis.append(g*base +r)
+			axis.append(g*base + r)
 	return axis
 
 rows = grid_layout(rBase)
@@ -61,7 +66,8 @@ def print_board(b):
 	line2  = expandLine("╟───┼───╫───╢")
 	line3  = expandLine("╠═══╪═══╬═══╣")
 	line4  = expandLine("╚═══╧═══╩═══╝")
-	nums2   = [ [""]+[symbol[n] for n in row] for row in b ]
+
+	nums2   = [ [""]+[symbol[n] for n in row] for row in b]
 	print(line0)
 	for r in range(1,side+1):
 		print( "".join(n+s for n,s in zip(nums2[r-1],line1.split("."))) )
@@ -69,13 +75,17 @@ def print_board(b):
 
 def puzzle():
 	
-	# produce board using randomized baseline pattern
+	# produce board using randomized baseline pattern function and shuffling function.
+	# pattern function is used as an indices assigner to select the placement of the 
+	# numbers chosen by nums.
+
 	board = [ [nums[pattern(r,c)] for c in cols] for r in rows ]
 
-	# copy the filled in board into board0
+	# This varialbe copies the original filled in board into board0
 	board0 = deepcopy(board)
 
-	# getting numbers out of our grid. 
+	# This part of the puzzle() function is randomly removing numbers from our original board. 
+	
 	squares = side*side
 	empties = squares * 3//4
 	for p in sample(range(squares),empties):
